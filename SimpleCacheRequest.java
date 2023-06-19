9b81147326b7c7dee15604cbe4bd166742510d11
nsdhl <dhlnischal10@gmail.com>
2023-06-19 07:55:11 +0545

    simplecache updated

---
+++ SimpleCacheRequest.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+public class SimpleCacheRequest extends CacheRequest {
+    private ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    public OutputStream getBody() throws IOException {
+        return out;
+    }
+
+    public void abort() {
+        out.reset();
+    }
+
+    public byte[] getData() {
+        if (out.size() == 0)
+            return null;
+        else
+            return out.toByteArray();
+    }
+}
